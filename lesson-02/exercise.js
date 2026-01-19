c09669efbe4d2a1f0b506cbb8a65aea22ddf4aee
rayhan123-ahmed <rmd09181@gmail.com>
2026-01-19 11:31:15 +0600

    question solve

---
+++ lesson-02/exercise.js
@@ -0,0 +1,53 @@
+                // question No. a
+const soup = 10
+const burger = 8
+const iceCream = 5
+
+const total = (1 * soup)+ (3*burger)+(1*iceCream)
+console.log(total);
+// total = 39
+                 // question No. b
+// Total Food price has comes from previous question
+  const peopleCount = 3
+
+  const costPerPerson = total / peopleCount
+  console.log(costPerPerson);
+//   per persion cost = 13
+
+                 // question No. c
+ const toaster = 1850
+ const shirts = 750     
+ 
+ const totalCost = (toaster + (shirts * 2)) / 100;
+ console.log(totalCost);
+ 
+                // question No. d
+const tax = 0.1
+
+const totalTex =(totalCost * tax)
+console.log(totalTex);
+
+                // question No. e
+const tex2 = 0.2
+
+const totalTex2 = (totalCost*tex2)
+console.log(totalTex2);
+
+        // setup for next some of question
+        const toasTer = 1899
+        const basketball = 2095
+        const tShirts = 799
+        const sheeping = 499
+        
+
+                // question No. f
+    const TotalPrice = (toasTer+basketball+tShirts)/100
+    console.log(TotalPrice);
+                // question No. g    
+    const beforeTex = TotalPrice+(sheeping/100)
+    console.log(beforeTex);
+                // question No. h 
+    const estimetedTex = (beforeTex*0.1).toFixed(2)
+    console.log(estimetedTex);
+   
+    
