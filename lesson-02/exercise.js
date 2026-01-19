11ade10e6de9f9816f0777240a8adbe91917d295
rayhan123-ahmed <rmd09181@gmail.com>
2026-01-19 20:26:10 +0600

    lesson-02 'exercise no. j,k,l,m,n

--- lesson-02/exercise.js
+++ lesson-02/exercise.js
@@ -49,5 +49,25 @@ console.log(totalTex2);
                 // question No. h 
     const estimetedTex = (beforeTex*0.1).toFixed(2)
     console.log(estimetedTex);
-   
+                // question No. j
+    Math.floor(2.8)                
+                // question No. K
+    Math.ceil(2.2)                
+                // question No. L
+    // C = 25°C
+    const celsius = 25
+    const fahrenheit = (celsius * 9/5)+32
+    console.log(fahrenheit);
+                // question No. M
+    // F = 86°F               
+    const fahrenheitM = 86
+    const celsiusM = (fahrenheitM-32)*5/9
+    console.log(celsiusM);
+                // question No. M  
+        // C = -5°C
+    const celsiusN = -5
+    const fahrenheitN = (celsiusN*9/5)+32
+    console.log(fahrenheitN);
     
+
+        
