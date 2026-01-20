c56942d310a15a858f5ff1f876a908d77385e0e8
rayhan123-ahmed <rmd09181@gmail.com>
2026-01-20 10:17:21 +0600

    lesson-03, 'exercise'

---
+++ lesson-03/exercise.js
@@ -0,0 +1,10 @@
+                // question No.3 a
+    const string = 'My name is: '
+    console.log(string);
+                // question No.3 a            
+    const myName = 'Rayhan'
+    console.log(myName);
+                // question No.3 a
+    const concatination = string +  myName
+    console.log(concatination);
+                    
