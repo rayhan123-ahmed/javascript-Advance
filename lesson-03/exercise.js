6f8cf7e12afc68974aa83664765c88a34ca83a84
rayhan123-ahmed <rmd09181@gmail.com>
2026-01-22 21:28:06 +0600

    lesson-03 exercise

--- lesson-03/exercise.js
+++ lesson-03/exercise.js
@@ -1,10 +1,30 @@
                 // question No.3 a
     const string = 'My name is: '
     console.log(string);
-                // question No.3 a            
+                // question No.3 b            
     const myName = 'Rayhan'
     console.log(myName);
-                // question No.3 a
+                // question No.3 c
     const concatination = string +  myName
     console.log(concatination);
-                    
+                // question No.3 d
+    const coffe = 5
+    const bagel = 3
+    const totalCost = 'total cost:$'+(coffe + bagel)
+    console.log(totalCost);
+    
+                // question No.3 e    
+    const total = coffe + bagel
+    const TotalCost = `total cost:${total}`
+    console.log(TotalCost);
+                // question No.3 f    
+    // alert(`total cost:${total}`) 
+
+                // question No.3 g
+    const coffee =    599
+    const bageel = 295
+    const cost = `total cost:$`+(coffe+bageel)/100
+    console.log(cost);
+                // question No.3 g
+    alert(`total cost:$${(599+295)/100}\nThank you come again`);
+    
