5ea4cb1508355883d3f36ade3b7b50d33c9670fe
rayhan123-ahmed <rmd09181@gmail.com>
2026-01-24 09:51:38 +0600

    lesson-05 exercise

---
+++ lesson-05/exercise.js
@@ -0,0 +1,22 @@
+// 5a
+    const name = 'Rayhan'
+// 5b
+    console.log(`my name is ${name}`);
+//  5c
+    const coffe = 1*5
+    const bagels = 2*3
+    const soup = 1*9
+    const cost = coffe+bagels+soup;
+    console.log(cost);
+// 5d
+    console.log(`food cost $${cost}`);
+// 5e
+    const tax = cost*0.1
+    console.log(tax);
+// 5f
+    console.log(`tax(10%) $${tax}`);
+// 5g
+   const totalCost = cost+tax
+   console.log(`Total cost $${totalCost}`);
+
+        
