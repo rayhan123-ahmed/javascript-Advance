e8ed562cc1b6bf5b5ff42ad4046b11af8ed06d5f
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-02-08 13:48:39 +0600

    Working on delivery option of the cart

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -3,12 +3,16 @@ export let cart = JSON.parse(localStorage.getItem('cart'))
 if (!cart) {
   cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
+  quantity:2,
+  deliveryDays: '1',
+  deliveryOptionId: '1'
 },{
-   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1,
+  deliveryDays: '2',
+  deliveryOptionId: '2'
 }]
-};
+}; 
 
   
 // Save cart in local storage 
@@ -35,7 +39,8 @@ export function addToCart(productId) {
         // push the object in a cart
         cart.push({
           productId:productId,
-          quantity: 1
+          quantity: 1,
+          deliveryOption:'1' 
         });
        }
        saveStorage()
