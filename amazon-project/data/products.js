c8f8cbea7e42821513b90c3b6ab255898471a312
Rayhan Ahmed Rifat <rmd09181@gmail.com>
2026-02-14 20:48:35 +0600

    Converts products into classes

--- amazon-project/data/products.js
+++ amazon-project/data/products.js
@@ -1,3 +1,7 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+
+
 export function getProduct(productId) {
     
     let matchingProduct;
@@ -10,7 +14,33 @@ export function getProduct(productId) {
  return matchingProduct
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
 
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  }
+
+  getStarsUrl (){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice(){
+      `$${formatCurrency(this.priceCents)}`
+  }
+}
+
+
+ 
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -670,4 +700,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+   return  new Product(productDetails)
+});
+
+
--- amazon-project/script/cheackout/orderSummary.js
+++ amazon-project/script/cheackout/orderSummary.js
@@ -48,7 +48,7 @@ const dateString = deliveryDate.format('dddd, MMMM D');
                 ${matchingProduct.name} 
             </div>
             <div class="product-price">
-                $${formatCurrency(matchingProduct.priceCents)}
+                ${matchingProduct.getPrice()}
             </div>
             <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                 <span>
